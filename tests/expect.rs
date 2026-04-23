607a7bbaf9f2f1914581972a3370f3870a4486ef
benmandrew <benmandrew@gmail.com>
2026-04-23 20:54:56 +0100

    feat: Check if Expect is installed

--- tests/expect.rs
+++ tests/expect.rs
@@ -1,14 +1,32 @@
 use std::fs;
 use std::process::Command;
+use std::process::Stdio;
+
+fn check_debug_build() {
+    let output = Command::new("cargo")
+        .args(&["build", "--quiet", "--bin", "tiber"])
+        .output()
+        .expect("failed to build tiber binary");
+    if !output.status.success() {
+        panic!("Failed to build tiber binary: {}", String::from_utf8_lossy(&output.stderr));
+    }
+}
+
+fn check_expect_installed() {
+    let expect_check = Command::new("expect")
+        .arg("-v")
+        .stdout(Stdio::null())
+        .stderr(Stdio::null())
+        .status();
+    if expect_check.is_err() || !expect_check.unwrap().success() {
+        panic!("Tcl Expect is not installed or not found in PATH.");
+    }
+}
 
 #[test]
 fn run_expect_scripts() {
-    let build_status = Command::new("cargo")
-        .args(&["build", "--quiet", "--bin", "tiber"])
-        .status()
-        .expect("failed to build tiber binary");
-    assert!(build_status.success(), "Failed to build tiber binary");
-
+    check_expect_installed();
+    check_debug_build();
     let scripts = fs::read_dir("tests/expect").unwrap();
     for entry in scripts {
         let path = entry.unwrap().path();
